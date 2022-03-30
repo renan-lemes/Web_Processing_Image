877e997e16a2e7111fe6b242765dad35b8778188
Renan Lemes Leepkaln <renan_ll@hotmail.com>
2022-03-29 22:38:35 -0300

    CHORES:arrumando bastante erros

--- flask_app/index.py
+++ flask_app/index.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # linka uma url a uma função do python
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
-    
