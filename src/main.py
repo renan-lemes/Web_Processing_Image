3d563e55e65192ecb0bf0773e01ca837f22b33bd
Renan Lemes Leepkaln <renan_ll@hotmail.com>
2022-04-04 10:02:46 -0300

    CHORES:Arrumando o main

--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@ from distutils.log import debug
 from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 
-DIRECTION = "C:\\Users\\Renan Lemes\\OneDrive\\Área de Trabalho\\Projeto_\\Web_Processing_Image\\src\\files"
+#DIRECTION = "C:\\Users\\Renan Lemes\\OneDrive\\Área de Trabalho\\Projeto_\\Web_Processing_Image\\src\\files"
 
 app = Flask(__name__)
 
 # linka uma url a uma função do python
 
-
+""" 
 @app.route("/", methods=["GET"])
 def lista_arquivo():
     arquivos = []
@@ -24,7 +24,7 @@ def lista_arquivo():
 
 @app.route("/<nome_do_arquivo>", methods=["POST"])
 def get_arquivo(nome_do_arquivo):
-    return send_from_directory(DIRECTION, nome_do_arquivo, as_attachment=True)
+    return send_from_directory(DIRECTION, nome_do_arquivo, as_attachment=True) """
 
 
 @app.route('/')
@@ -34,4 +34,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
-    render_template("index.html")
