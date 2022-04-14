379685852b7fda03a42e6f0a051ae8bc72b5df98
Renan Lemes Leepkaln <renan_ll@hotmail.com>
2022-04-13 23:00:03 -0300

    CHORES:Consegui aplica a imagem

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ from flask import Flask, render_template, request, jsonify, send_from_directory,
 import os
 import jyserver.Flask as jsf
 #DIRECTION = "C:\\Users\\Renan Lemes\\OneDrive\\Área de Trabalho\\Projeto_\\Web_Processing_Image\\src\\assets"
-DIRECTION = os.getcwd() + '/assets'
+DIRECTION = os.getcwd() + '/static/img'
 
 app = Flask(__name__)
 
 
-@app.route("/assets", methods=["GET"])
+@app.route("/img", methods=["GET"])
 def get_img():
     arquivos = []
 
@@ -20,12 +20,12 @@ def get_img():
     return jsonify(arquivos)
 
 
-@app.route("/assets/<nome_do_arquivo>", methods=["GET"])
+@app.route("/img/<nome_do_arquivo>", methods=["GET"])
 def get_arquivo(nome_do_arquivo):
     return send_from_directory(DIRECTION, nome_do_arquivo, as_attachment=True)
 
 
-@app.route("/assets", methods=["POST"])
+@app.route("/img", methods=["POST"])
 def post_img():
     arquivo = request.files.get("meuArquivo")
     print(arquivo)
@@ -37,13 +37,13 @@ def post_img():
 @jsf.use(app)
 class App:
     def __init__(self):
-        arquivos = []
+
         for nome_do_arquivo in os.listdir(DIRECTION):
             endereco_do_arquivo = os.path.join(DIRECTION, nome_do_arquivo)
             if(os.path.isfile(endereco_do_arquivo)):
-                arquivos.append(nome_do_arquivo)
+                arquivos = nome_do_arquivo
 
-        self.img = arquivos[0]
+        self.img = arquivos
         print(self.img)
 
     def add_img(self):
